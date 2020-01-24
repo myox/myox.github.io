30195a44b2b4e9bc19ad4a04dc9c43c33e088157
Unknown <markyoxon@gmail.com>
2020-01-24 10:43:16 +0000

    Dialog edit 5
    Added openPayLoad

--- js/dialog.js
+++ js/dialog.js
@@ -1,14 +1,14 @@
- $(document).ready(function () {
+$(document).ready(function () {
     // The only difference between an extension in a dashboard and an extension
     // running in a popup is that the popup extension must use the method
     // initializeDialogAsync instead of initializeAsync for initialization.
     // This has no affect on the development of the extension but is used internally.
-    tableau.extensions.initializeDialogAsync().then(function () {
-
+    tableau.extensions.initializeDialogAsync().then(function (openPayLoad) {
+        
         $("#configSubmit").click(function () {
             
             var dbName = $("#config-dbName").val()
             tableau.extensions.ui.closeDialog(dbName);
         });
     });
-  }); 
+}); 
