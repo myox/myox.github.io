d6d743556e20ea6a87544e7dabcb4e6fee22e598
Unknown <markyoxon@gmail.com>
2020-01-24 10:30:17 +0000

    Dialog edit 4

--- js/app.js
+++ js/app.js
@@ -1,46 +1,50 @@
-$(document).ready(function() {
+'use strict';
 
-    tableau.extensions.initializeAsync({'configure': configure}).then(function() {
-    
-        }, function(err) {
-                alert("Error while Initializing: " + err.toString());
-            });
+(function () {
 
-});
+    $(document).ready(function() {
+
+        tableau.extensions.initializeAsync({'configure': configure}).then(function() {
         
-
-function configure () {
-
-    const popupUrl = 'https://myox.github.io/dialog.html';
-
-    tableau.extensions.ui.displayDialogAsync(popupUrl, { height: 500, width: 500 }).then((closePayload) => {
-    
-        $('#inactive').hide();
-        $('#active').show();
-
-        var dashboard = closePayload;
-        var sheet = tableau.extensions.dashboardContent.dashboard.name;
-    
-        $("#submitBtn").click(function(){
-            var text = $("#bodyTxt").val();
-            var url = 'https://script.google.com/a/my.shu.ac.uk/macros/s/AKfycbxWglAuwM3dkF2rFDc4zDXcHUuXyn3EgtVvaar1kSnKG4qalkGf/exec';
-
-            $.post(url,{data: text, data2: sheet, data3: dashboard});
-            
-        }); 
-    
-    // configuration error 
-    }).catch((error) => {
-        // One expected error condition is when the popup is closed by the user (meaning the user
-        // clicks the 'X' in the top right of the dialog).  This can be checked for like so:
-        switch (error.errorCode) {
-            case tableau.ErrorCodes.DialogClosedByUser:
-            console.log('Dialog was closed by user');
-            break;
-            default:
-            alert("Error during configuration: " + error.message.toString());
-        }
+            // }, function(err) {
+            //         alert("Error while Initializing: " + err.toString());
+            //     });
+        });
     });
-}
+            
 
+    function configure () {
+
+        const popupUrl = 'https://myox.github.io/dialog.html';
+
+        tableau.extensions.ui.displayDialogAsync(popupUrl, { height: 500, width: 500 }).then((closePayload) => {
+        
+            $('#inactive').hide();
+            $('#active').show();
+
+            var dashboard = closePayload;
+            var sheet = tableau.extensions.dashboardContent.dashboard.name;
+        
+            $("#submitBtn").click(function(){
+                var text = $("#bodyTxt").val();
+                var url = 'https://script.google.com/a/my.shu.ac.uk/macros/s/AKfycbxWglAuwM3dkF2rFDc4zDXcHUuXyn3EgtVvaar1kSnKG4qalkGf/exec';
+
+                $.post(url,{data: text, data2: sheet, data3: dashboard});
+                
+            }); 
+        
+        // configuration error 
+        }).catch((error) => {
+            // One expected error condition is when the popup is closed by the user (meaning the user
+            // clicks the 'X' in the top right of the dialog).  This can be checked for like so:
+            switch (error.errorCode) {
+                case tableau.ErrorCodes.DialogClosedByUser:
+                console.log('Dialog was closed by user');
+                break;
+                default:
+                console.error(error.message);
+            }
+        });
+    }
+})();
 
